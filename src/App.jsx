4c34fdfd912db8441154294d830a02cf321e8f25
Ta Toan <tttoan1801@gmail.com>
2024-09-12 17:04:21 +0700

    dark-light-mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,77 @@
+import { useColorScheme } from '@mui/material/styles';
+// import useMediaQuery from '@mui/material/useMediaQuery';
 import { Button } from '@mui/material';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import LightModeIcon from '@mui/icons-material/LightMode';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness';
+import Box from '@mui/material/Box';
+
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme();
+  const handleChange = (event) => {
+    const selectedMode = event.target.value;
+    setMode(selectedMode);
+  };
+
+  return (
+    <FormControl
+      sx={{ m: 1, minWidth: 120 }}
+      size="small">
+      <InputLabel id="lable-select-dark-light-mode">Mode</InputLabel>
+      <Select
+        labelId="lable-select-dark-light-mode"
+        id="select-dark-light-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}>
+        <MenuItem value="light">
+          <Box sx={{ display: 'flex', alignItem: 'center', gap: 1 }}>
+            <LightModeIcon fontSize="small" />
+            <span>Light</span>
+          </Box>
+        </MenuItem>
+        <MenuItem value="dark">
+          <Box sx={{ display: 'flex', alignItem: 'center', gap: 1 }}>
+            <DarkModeOutlinedIcon fontSize="small" />
+            <span>Dark</span>
+          </Box>
+        </MenuItem>
+        <MenuItem value="system">
+          <Box sx={{ display: 'flex', alignItem: 'center', gap: 1 }}>
+            <SettingsBrightnessIcon fontSize="small" />
+            <span>System</span>
+          </Box>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  );
+}
+
+function ModeToggle() {
+  // const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  // const prefersLightMode = useMediaQuery('(prefers-color-scheme: light)');
+  const { mode, setMode } = useColorScheme();
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light');
+      }}>
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  );
+}
 
 function App() {
   return (
     <>
+      <ModeSelect />
+      <hr />
+      <ModeToggle />
+      <hr />
       <Button variant="contained">Hello world</Button>
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import CssBaseline from '@mui/material/CssBaseline';
+import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles';
+import theme from './theme';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <CssBaseline />
-    <App />
+    <CssVarsProvider theme={theme}>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      <App />
+    </CssVarsProvider>
   </StrictMode>
 );
