4a73f0b0c3880ff64f12cd15e7820240b33f17b4
Ta Toan <tttoan1801@gmail.com>
2024-09-13 11:00:20 +0700

    date 13 9

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -1,6 +1,7 @@
 import Container from '@mui/material/Container';
-import ModeSelect from '../../components/ModeSelect/index';
-import Box from '@mui/material/Box';
+import AppBar from '../../components/AppBar/index';
+import BoardBar from './BoardBar';
+import BoardContent from './BoardContent';
 
 const Board = () => {
   return (
@@ -8,35 +9,9 @@ const Board = () => {
       disableGutters
       maxWidth={false}
       sx={{ height: '100vh' }}>
-      <Box
-        sx={{
-          backgroundColor: 'primary.light',
-          width: '100%',
-          height: (theme) => theme.trello.appBarHeight,
-          display: 'flex',
-          alignItems: 'center',
-        }}>
-        <ModeSelect />
-      </Box>
-      <Box
-        sx={{
-          backgroundColor: 'primary.dark',
-          width: '100%',
-          height: (theme) => theme.trello.boardBarHeight,
-          display: 'flex',
-          alignItems: 'center',
-        }}>
-        Board Bar
-      </Box>
-      <Box
-        sx={{
-          backgroundColor: 'primary.main',
-          width: '100%,',
-          height: (theme) =>
-            `calc(100vh - ${theme.trello.appBarHeight} - ${theme.trello.boardBarHeight})`,
-        }}>
-        Board Content
-      </Box>
+      <AppBar />
+      <BoardBar />
+      <BoardContent />
     </Container>
   );
 };
